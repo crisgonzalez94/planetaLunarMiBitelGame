f650fdbe2539ec55bf4752a91bf3dc7c339bacf2
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2022-03-27 12:27:56 -0300

    firstCommit

---
+++ src/carouselSwiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    effect: "fade",
+    centeredSlides: true,
+    autoplay: {
+      delay: 3750,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    /*
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },*/
+  });
